0c6d2f99d206214d4a4a6045846d9e0331aa565f
LemonCat <95578425+Lem0nCat@users.noreply.github.com>
2026-06-19 16:30:39 +0300

    add extension type detector func in utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"bytes"
 	"io"
+	"mime"
 	"net/http"
 )
 
@@ -20,4 +21,24 @@ func DetectContentType(file io.Reader) (string, io.Reader, error) {
 	// Склеиваем обратно
 	newReader := io.MultiReader(bytes.NewReader(buffer[:n]), file)
 	return contentType, newReader, nil
+}
+
+func MapContentTypeToExt(contentType string) string {
+	switch contentType {
+	case "image/jpeg":
+		return ".jpg"
+	case "image/jpg":
+		return ".jpg"
+	case "image/png":
+		return ".png"
+	case "image/gif":
+		return ".gif"
+	case "image/webp":
+		return ".webp"
+	default:
+		if exts, err := mime.ExtensionsByType(contentType); err == nil && len(exts) > 0 {
+			return exts[0]
+		}
+		return ""
+	}
 }
